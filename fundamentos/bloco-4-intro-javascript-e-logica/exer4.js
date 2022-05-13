f4d9533bd25f63aa01902c33d170ea3e711a76f6
SandraPer <sandrasilvapereira329@gmail.com>
2022-05-13 01:15:16 -0300

    Exercicios do 1 ao 11

---
+++ fundamentos/bloco-4-intro-javascript-e-logica/exer4.js
@@ -0,0 +1,9 @@
+const a = 8;
+
+if (a < 0) {
+  console.log("Negativo");
+} else if (a > 0) {
+  console.log("Positivo");
+} else {
+  console.log("zero");
+}
---
+++ fundamentos/bloco-4-intro-javascript-e-logica/exer5.js
@@ -0,0 +1,10 @@
+const pontoA = 60;
+const pontoB = 60;
+const pontoC = 60;
+
+let somaDosAngulos = pontoA + pontoB + pontoC;
+if ((somaDosAngulos = 180)) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
